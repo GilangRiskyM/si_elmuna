54dd87e6fd14ac801c387bcc41b59ac63630b66f
Gilang Risky M <gilangriskymahardhika@gmail.com>
2024-08-01 10:41:59 +0700

    implementasi softDelete pada model

--- app/Models/BahasaInggris.php
+++ app/Models/BahasaInggris.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BahasaInggris extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'daftar_bahasa_inggris';
 

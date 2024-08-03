89fe83f462fb04bdb296856ba215a5f9b0009fac
Gilang Risky M <gilangriskymahardhika@gmail.com>
2024-08-03 11:37:47 +0700

    ubah login + antarmuka dashboard admin

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -2,70 +2,96 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title')</title>
-    <link rel="icon" href="/asset/img/icon1.png" type="image/x-icon">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Data Kursus | @yield('title')</title>
+    <link rel="icon" href="{{ asset('asset/img/icon1.png') }}" type="image/x-icon">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
     <link rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <style>
-        .container {
-            min-height: 75vh;
-        }
-    </style>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <script src="{{ asset('js/scripts.js') }}"></script>
 </head>
 
-<body>
-    <div class="min-vh-100 justify-content-between">
-        {{-- Navbar  --}}
-        <nav class="navbar navbar-dark navbar-expand-lg bg-dark sticky-top">
-            <div class="container-fluid">
-                <div class="d-flex align-items-center">
-                    <a href="/dashboard" data-aos="fade" class="">
-                        <img src="/asset/img/elmuna2.png" alt="Logo" width="40%">
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <!-- Navbar Brand-->
+        <a class="navbar-brand ps-3" href="/dashboard"><img src="{{ asset('asset/img/icon1.png') }}" alt=""
+                width="40" height="40"> {{ Auth::user()->name }}</a>
+        <!-- Sidebar Toggle-->
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!">
+            <i class='bx bx-menu'></i>
+        </button>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Menu</div>
+                        <a class="nav-link" href="/dashboard">
+                            <div class="sb-nav-link-icon">
+                                <i class='bx bxs-dashboard'></i>
+                            </div>
+                            Dashboard
+                        </a>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#data_peserta" aria-expanded="false" aria-controls="data_peserta">
+                            <div class="sb-nav-link-icon"><i class='bx bxs-gas-pump'></i></div>
+                            Data Peserta Kursus
+                            <div class="sb-sidenav-collapse-arrow">
+                                <i class='bx bxs-chevron-down'></i>
+                            </div>
+                        </a>
+                        <div class="collapse" id="data_peserta" aria-labelledby="headingOne"
+                            data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="/data_mengemudi">Kursus Mengemudi</a>
+                                <a class="nav-link" href="/data_komputer">Kursus Komputer</a>
+                                <a class="nav-link" href="/data_digital_marketing">Kursus Digital Marketing</a>
+                                <a class="nav-link" href="/data_bahasa_inggris">Kursus Bahasa Inggris</a>
+                                <a class="nav-link" href="/data_pemrograman">Kursus Pemrograman</a>
+                                <a class="nav-link" href="/data_desain_grafis">Kursus Desain Grafis</a>
+                                <a class="nav-link" href="/data_video_editing_fotografi">
+                                    Kursus Video Editing dan Fotografi
+                                </a>
+                            </nav>
+                        </div>
+                        <a href="/logout" class="nav-link">
+                            <div class="sb-nav-link-icon"><i class='bx bx-log-out bx-flip-horizontal'></i></div>
+                            Keluar
+                        </a>
+                    </div>
                 </div>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="/dashboard">Dashboard</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/logout">Logout</a>
-                        </li>
-                    </ul>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Login sebagai</div>
+                    {{ Auth::user()->name }}
                 </div>
-            </div>
-        </nav>
-
-        <div class="container my-3">
-            @yield('content')
+            </nav>
         </div>
-
-        {{-- Footer  --}}
-        <footer class="py-5 bg-dark">
-            <div>
-                <p class="m-0 text-center text-white">Copyright &copy; Elmuna Kebumen {{ date('Y') }}</p>
-            </div>
-        </footer>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    @yield('content')
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Elmuna Kebumen {{ date('Y') }}</div>
+                    </div>
+                </div>
+            </footer>
+        </div>
     </div>
-
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script>
-        AOS.init();
-    </script>
     @stack('js')
 </body>
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,13 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Elmuna| Login</title>
+    <title>Elmuna | Login</title>
     <link rel="icon" href="{{ asset('asset/img/icon1.png') }}" type="image/x-icon">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body class="bg-info">
@@ -24,60 +22,54 @@
                             <div class="card shadow-lg border-0 rounded-lg mt-5">
                                 <div class="card-header">
                                     <center>
-                                        <img src="/asset/img/elmuna.png" alt="Gambar gagal dimuat..." width="100%"
-                                            height="auto">
+                                        <a href="/">
+                                            <img src="/asset/img/elmuna.png" alt="Logo" width="50%">
+                                        </a>
                                     </center>
-                                    <h5 class="text-center font-weight-light my-1">Sistem Pendaftaran Kursus Elmuna</h5>
+                                    <h5 class="text-center font-weight-light my-1">
+                                        Sistem Pendaftaran Kursus Elmuna Kebumen
+                                    </h5>
                                 </div>
                                 <div class="card-body">
-                                    <center>
-                                        <form action="" method="POST">
-                                            @csrf
-                                            @if (Session::has('status'))
-                                                <div class="alert alert-danger alert-dismissible fade show"
-                                                    role="alert">
-                                                    {{ Session::get('message') }}
-                                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                                        aria-label="Close"></button>
-                                                </div>
-                                            @endif
-                                            <div class="input-group mb-3">
-                                                <div class="form-floating">
-                                                    <input type="email" class="form-control" id="email"
-                                                        placeholder="" name="email" required>
-                                                    <label for="email">Email</label>
-                                                </div>
+                                    <form action="" method="POST">
+                                        @csrf
+                                        @if (Session::has('status'))
+                                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                                {{ Session::get('message') }}
+                                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                                    aria-label="Close"></button>
                                             </div>
-                                            <div class="input-group mb-3">
-                                                <div class="form-floating">
-                                                    <input type="password" class="form-control" id="pw"
-                                                        placeholder="" name="password" required>
-                                                    <label for="pw">Password</label>
-                                                </div>
+                                        @endif
+                                        <div class="input-group mb-3">
+                                            <div class="form-floating">
+                                                <input type="email" class="form-control" id="email" placeholder=""
+                                                    name="email">
+                                                <label for="email">Email</label>
                                             </div>
-                                            <div class="mb-3">
-
+                                        </div>
+                                        <div class="input-group mb-3">
+                                            <div class="form-floating">
+                                                <input type="password" class="form-control" id="pw"
+                                                    placeholder="" name="password">
+                                                <label for="pw">Password</label>
+                                            </div>
+                                        </div>
+                                        <div class="mb-3">
+                                            <center>
                                                 <button type="reset" class="btn btn-danger mx-2">Batal</button>
                                                 <button type="submit" class="btn btn-success mx-2">Login</button>
-
-                                            </div>
-                                        </form>
-                                    </center>
+                                            </center>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-
-
             </main>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    </script>
 </body>
 
 </html>

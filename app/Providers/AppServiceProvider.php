2d43e65a074e02ede6a8e2d051ced9a9f61e743e
Gilang Risky M <gilangriskymahardhika@gmail.com>
2025-01-02 16:10:57 +0700

    update vercel.json

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use URL;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+        URL::forceScheme('https');
     }
 }
